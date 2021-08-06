2f5ff8648da12b1f3d68f68d8d709d9d8c6b2b44
Swarnava Chakraborty <swarnava415@gmail.com>
2021-08-06 18:46:16 +0530

    listview and header in services activity - day-2

--- app/src/main/java/adosy/edu/myapp/CoursesActivity.java
+++ app/src/main/java/adosy/edu/myapp/CoursesActivity.java
@@ -10,5 +10,8 @@ public class CoursesActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_courses);
+        getSupportActionBar().hide();
+
+
     }
 }
--- app/src/main/java/adosy/edu/myapp/ServicesActivity.java
+++ app/src/main/java/adosy/edu/myapp/ServicesActivity.java
@@ -2,13 +2,67 @@ package adosy.edu.myapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.Image;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 
 public class ServicesActivity extends AppCompatActivity {
+
+    int content_id[] = { R.drawable.banner1, R.drawable.banner2, R.drawable.banner3};
+    int noOfContent = 0, loopi=0;
+    ImageView imageView;
+
+    LinearLayout linear;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_services);
+        getSupportActionBar().hide();
+
+        imageView = findViewById(R.id.header_content);
+        noOfContent = content_id.length;
+
+        linear = findViewById(R.id.linear);
+
+
+        loopAnim();
+
+    }
+
+
+    void loopAnim(){
+        Runnable r = new Runnable() {
+            @Override
+            public void run() {
+
+
+                if(noOfContent-1==loopi)
+                    loopi = 0;
+                else
+                    loopi++;
+
+                imageView.setImageResource(content_id[loopi]);
+
+                loopAnim();
+
+
+                //body
+            }
+        };
+        Handler h = new Handler();
+        h.postDelayed(r,2500);
+    }
+
+
+    public void click(View view) {
+        if (linear.getVisibility() == View.GONE)
+            linear.setVisibility(View.VISIBLE);
+        else
+            linear.setVisibility(View.GONE);
     }
 }
